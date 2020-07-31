0b2693aaadaed9e80db901b07beaa31ecbeaae7f
rafael <rafaelnepomuceno00@gmail.com>
2020-07-31 19:16:55 -0300

    tela das camisetas prontas em lista e em grade

---
+++ lib/screens/products_screen.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:loja2/data/product_data.dart';
+import 'package:loja2/tiles/products_tile.dart';
+
+class CaregoryScreen extends StatelessWidget {
+  final DocumentSnapshot snapshot;
+
+  CaregoryScreen(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(snapshot.data['title']),
+          centerTitle: true,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.grid_on),
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+              ),
+            ],
+          ),
+        ),
+        body: FutureBuilder<QuerySnapshot>(
+            future: Firestore.instance
+                .collection('products')
+                .document(snapshot.documentID)
+                .collection('itens')
+                .getDocuments(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData)
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              else
+                return TabBarView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      GridView.builder(
+                          padding: EdgeInsets.all(4.0),
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 4,
+                            crossAxisSpacing: 4,
+                            childAspectRatio: 0.65,
+                          ),
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (context, index) {
+                            return ProductTile(
+                                "grid",
+                                ProductData.fromDoCument(
+                                    snapshot.data.documents[index]));
+                          }),
+                      ListView.builder(
+                        padding: EdgeInsets.all(4.0),
+                        itemCount: snapshot.data.documents.length,
+                        itemBuilder: (context, index) {
+                          return ProductTile(
+                              "list",
+                              ProductData.fromDoCument(
+                                  snapshot.data.documents[index]));
+                        },
+                      )
+                    ]);
+            }),
+      ),
+    );
+  }
+}
---
+++ lib/tiles/products_tile.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:loja2/data/product_data.dart';
+import 'package:loja2/screens/product_screen.dart';
+
+class ProductTile extends StatelessWidget {
+  final String type;
+  final product;
+
+  ProductTile(this.type, this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => ProductScreen(product)));
+      },
+      child: Card(
+        child: type == 'grid'
+            ? Column(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+              )
+            : Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                      height: 250.0,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+      ),
+    );
+  }
+}
